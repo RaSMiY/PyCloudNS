9217f56c2c298ba27dc10994756c4653279df40a
Vyacheslav Anzhiganov <vanzhiganov@ya.ru>
2016-07-16 04:50:52 +0300

    version 2.0.5

--- setup.py
+++ setup.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
-from setuptools import setup
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
 
 setup(
     name='PyCloudNS',
-    version='2.0.2',
+    version='2.0.5',
+    description='Python Lib for DNS API on CloudNS.ru',
+    long_description=long_description,
+    url='http://cloudns.ru',
     author='Vyacheslav Anzhiganov',
     author_email='hello@anzhiganov.com',
+    license='MIT',
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     packages=[
         'PyCloudNS'
     ],
     install_requires=[
         'requests',
+    ],
+    keywords='public dns service',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
     ],
 )
